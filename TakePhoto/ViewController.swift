7c18898ea0fbadb0a922f60426a4a0bb23c4eac8
Katariina Martikainen <KataMar@n191-p59.eduroam.kth.se>
2018-04-13 14:15:49 +0200

    debugged camera crash

--- TakePhoto/ViewController.swift
+++ TakePhoto/ViewController.swift
@@ -8,8 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
+    @IBOutlet weak var PhotoLibrary: UIButton!
+    
+    @IBOutlet weak var Camera: UIButton!
+    
+    @IBOutlet weak var ImageDisplay: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func PhotoLibraryAction(_ sender: UIButton) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
+    @IBAction func CameraAction(_ sender: UIButton) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .camera
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        ImageDisplay.image = info[UIImagePickerControllerOriginalImage] as? UIImage;dismiss(animated: true, completion: nil)
+    }
+    
 }
 
